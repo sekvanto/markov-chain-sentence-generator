bec38a7f5d3584214eed3b549755c6d30ed4077e
sekvanto <zip.aioniplaitnereffid@gmail.com>
2021-03-28 11:58:02 +0300

    Added two newlines

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main () {
     getDictionary(dict, filePath);
     int mode = chooseOption();
     if (mode == NORMAL)
-        cout << "\n" << SEPARATOR << clean(getSentence(dict)) << "\n\n";
+        cout << << SEPARATOR << "\n" << clean(getSentence(dict)) << "\n\n";
     else
         doShitPost(dict);
     return 0;
